f23675e2d6c4117f395d3eab8c6d15bc588b13ea
samanthamarberger <slatham04@gmail.com>
2023-07-24 12:19:22 -0600

    commit tree 7442e2036c5e9ec4ccb92d594ae989c6e53d4aea

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,9 +1,14 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def index
+    reviews = Review.all.order(rating: :desc)
+    render json: reviews, include: :dog_house
+  end
+  
   def show
     dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+    render json: dog_house, include: :reviews
   end
 
   private
